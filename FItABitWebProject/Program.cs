d2eaa751b2c409236a97163a86c66289f708d843
Krisbor3 <krisborr@gmail.com>
2022-03-10 19:07:10 +0200

    Added Users to the Db

--- FItABitWebProject/Program.cs
+++ FItABitWebProject/Program.cs
@@ -1,4 +1,4 @@
-using FItABitWebProject.Data;
+using FitABit.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
--- FitABit.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ FitABit.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace FItABitWebProject.Data.Migrations
+namespace FitABit.Infrastructure.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
